48177c15450258ca3b71e8c8f74837e13a73bc93
Garcia Roman, Eduardo <egarciar@iu.edu>
2021-10-26 15:44:57 -0400

    Template Changes
    updated for changing template arrows

--- Tap Revolution/Assets/TemplateArrowKeys.cs
+++ Tap Revolution/Assets/TemplateArrowKeys.cs
@@ -3,35 +3,85 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TemplateArrowKeys : MonoBehaviour
+    
 {
+    [SerializeField] GameObject leftArrow;
+    [SerializeField] GameObject rightArrow;
+    [SerializeField] GameObject upArrow;
+    [SerializeField] GameObject downArrow;
+    public SpriteRenderer spriteRender;
+    public Sprite[] whiteSprite;
+    public Sprite[] blackSprite;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+
+    /*
+     * SPRITE CHANGES BELOW ARE LEARNED FROM THIS TUTORIAL WEBPAGE
+     * https://gamedevbeginner.com/how-to-change-a-sprite-from-a-script-in-unity-with-examples/
+     * 
+     */
+
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("left")) {
+        if (Input.GetKeyDown("left")) {
 
             print("left arrow key");
-
-            //Below code destroys all template arrows
-            //Destroy(gameObject);
-
-            //Looking for code that destroys one template arrow... just to start off figuring out how to access
-            //separate arrows
+            spriteRender.sprite = whiteSprite[1];
+            
         }
-        else if(Input.GetKeyDown("up")){
+        if(Input.GetKeyDown("up")){
             print("up arrow key");
+            spriteRender.sprite = whiteSprite[3];
+            
         }
-        else if(Input.GetKeyDown("down")){
+        if(Input.GetKeyDown("down")){
             print("down arrow key");
 
+            spriteRender.sprite = whiteSprite[0];
+            
+
         }
-        else if(Input.GetKeyDown("right")){
+        if(Input.GetKeyDown("right")){
             print("right arrow key");
+
+            spriteRender.sprite = whiteSprite[2];
+            
+
+        }
+        if (Input.GetKeyUp("left"))
+        {
+
+            print("left arrow key up");
+            spriteRender.sprite = blackSprite[1];
+            
+        }
+        if (Input.GetKeyUp("up"))
+        {
+            print("up arrow key up");
+            spriteRender.sprite = blackSprite[3];
+            
+        }
+        if (Input.GetKeyUp("down"))
+        {
+            print("down arrow key up");
+
+            spriteRender.sprite = blackSprite[0];
+            
+
+        }
+        if (Input.GetKeyUp("right"))
+        {
+            print("right arrow key up");
+
+            spriteRender.sprite = blackSprite[2];
+            
 
         }
     }
